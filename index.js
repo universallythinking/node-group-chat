6661874a6a323a25691455d15b00f186e0e4caff
Ben <Benjamin@universallthinking.com>
2021-09-07 13:57:30 -0400

    Initial

--- index.js
+++ index.js
@@ -7,6 +7,12 @@ const io = require('socket.io')(http);
 const { translate } = require('free-translate');
 
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.get('/', function(req, res) {
     res.render('index.ejs');
 });
